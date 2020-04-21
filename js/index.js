65a5cf49c5d42f284358801deb3a587be5aa9b75
chzeng03 <60713966+chzeng03@users.noreply.github.com>
2020-04-21 11:20:26 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const eventlist = document.querySelector('.Events');
+const loggedoutlinks = document.querySelectorAll('.logged-out');
+const loggedinlinks = document.querySelectorAll('.logged-in');
+
+const setupUI = (user) => {
+    if (user) {
+
+        loggedinlinks.forEach(item => item.style.display = 'block');
+        loggedoutlinks.forEach(item => item.style.display = 'none');
+
+    } else {
+
+        loggedinlinks.forEach(item => item.style.display = 'none');
+        loggedoutlinks.forEach(item => item.style.display = 'block');
+    }
+}
+
+//setup events
+const setupEvents = (data) => {
+
+    let html = '';
+    data.forEach(doc => {
+        const event = doc.data();
+ 
+        const li = `
+            <li>
+                <div class="collapsible-header grey lighten-4" style="font-weight:bold">${event.Name}</div>
+                <div class="collapsible-body white">${event.Time} at ${event.Location}</div>
+            </li>
+            `;
+
+        html += li
+    });
+
+    eventlist.innerHTML = html;
+}
+
+
+// setup materialize componets
+document.addEventListener('DOMContentLoaded', function () {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+});
---
+++ js/js/adminindex.js
@@ -0,0 +1,20 @@
+// Add Admin function
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeapp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    //get user and add custom claim(admin)
+
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            admin: true
+        });
+    }).then(() => {
+        return {
+            message: `Success! ${data.email} has been made an admin`
+        }
+    }).catch(err => {
+        return err;
+    });
+});
